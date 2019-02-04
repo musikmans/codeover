271c883c2659d6492fe42c781211a5e7e7bb2d5b
Mani Jafari <manielli@gmail.com>
2019-02-03 19:51:09 -0800

    Minor fix

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -16,27 +16,10 @@ class ResultsController < ApplicationController
                 UserAnswer.create(question_id: q_id, quiz_id: params[:quiz_id], answer_id: params[key], user: current_user)
             end
         end
-        
-        puts arr
-        # params[:iteration].to_i.times do
-        #     @user_answer = UserAnswer.create(quiz_id: params[:quiz_id], question_id: , answer_id: params[:answer_id], user: current_user)
-        # end
-
         redirect_to results_path
     end
 
     def index
-    end
-
-
-    private
-    def permit!
-        each_pair do |key, value|
-          convert_hashes_to_parameters(key, value)
-          self[key].permit! if self[key].respond_to? :permit!
-        end
-      
-        @permitted = true
-        self
+    
     end
 end
