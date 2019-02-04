89ef1b9cadd296cbbeac580470f74951e9da414e
François Tremblay <francois@ftremblay.net>
2019-02-04 09:33:07 -0800

    restrict show page to user only, redesigned result page

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,6 @@
 class QuizzesController < ApplicationController
     before_action :find_quiz, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, except: [:index, :show]
+    before_action :authenticate_user!, except: [:index]
     before_action :authorize_user!, only: [:edit, :update, :destroy]
 
     def new
